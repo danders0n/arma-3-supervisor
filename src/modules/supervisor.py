88f7709e33fd01ac5605f2b898aca5e181b98a6a
David Anderson <dr.d4vid.4nders0n@gmail.com>
2025-08-30 23:32:20 +0200

    Initial Commit
    Arma-3-Server-Super-Supervisor - A3S

---
+++ src/modules/supervisor.py
@@ -0,0 +1,13 @@
+
+
+class Supervisor():
+    """
+    docstring
+    """
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    supervisor = Supervisor()
+
+    print(supervisor)
